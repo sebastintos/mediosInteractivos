4cf487871cd57a82380ef068d621c64b52ac7b53
sebastintos <35973374+sebastintos@users.noreply.github.com>
2018-01-30 14:46:56 -0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,140 @@
+function setup() { 
+  createCanvas(500, 320);//tamaño de papes	
+  background(255,255,255);//color de papel
+	
+
+	
+	strokeWeight(3);//tamaño pincel	
+	stroke(0,0,0);//Color Pincel
+  line(55,205,370,322);
+  line(65,128,382,322);
+
+
+	
+	strokeWeight(5);//tamaño pincel	
+	stroke(0,0,0);//Color Pincel
+	line(427,0,427,250); //dibujo linea
+  line(440,0,440,250);
+  strokeWeight(2)
+  line(400,55,470,55);
+  line(405,60,465,60);
+  line(405,220,465,220);
+  line(405,223,465,223);
+  line(405,227,465,227);
+  
+  strokeWeight(2)
+  fill(161,161,176)
+  ellipse(146,126,50,50);
+  
+  strokeWeight(4)
+  fill(209,47,84)
+  ellipse(40,270,35,35);
+  
+ 
+  
+  strokeWeight(2)
+  fill(255,255,255)
+  ellipse(240,320,40,40)
+
+  point(242,309);
+  point(247,308);
+  point(239,305);
+  point(235,312);
+  point(230,318);
+  point(250,310);
+  point(242,315);
+  point(247,310);
+  point(239,315);
+  point(225,309);
+  point(225,317);
+  point(250,319);
+  point(232,305);
+  point(230,309);
+  
+  
+  strokeWeight(0)
+  fill(230,214,23)
+  beginShape();
+  vertex(255,177);
+  vertex(305,110);
+  vertex(344,177);
+  endShape(CLOSE);
+	
+  strokeWeight(5)
+  stroke(0,0,0)
+  line(160,320,242,30)
+  line(242,30,330,320)
+  
+  strokeWeight(1)
+  stroke(0,0,0)
+  fill(255,255,255)
+  ellipse(371,197,53,53)
+  
+  strokeWeight(1)
+  stroke(0,0,0)
+  fill(255,255,255)
+  ellipse(371,197,51,51)
+  
+  strokeWeight(1)
+  stroke(0,0,0)
+  fill(255,255,255)
+  ellipse(371,197,48,48)
+  
+  
+  strokeWeight(1)
+  stroke(0,0,0)
+  fill(255,255,255)
+  ellipse(371,197,45,45)
+  
+  
+  strokeWeight(1)
+  stroke(0,0,0)
+  fill(49,43,135)
+  ellipse(371,197,40,40);
+  
+  
+  
+  
+  stroke(210,124,147)
+  fill(210,124,147)
+  rect(438,300,70,20);
+  
+  fill(191,14,51)
+  triangle(470,320,500,310,500,320);
+  
+  strokeWeight(2)
+  stroke(0,0,0)
+  line(470,320,500,310);
+  
+  strokeWeight(1)
+  stroke(0,0,0)
+  noFill()
+  ellipse(440,110,50,50)
+  ellipse(440,110,55,55)
+  
+  noStroke()
+  fill(255,255,255)
+  rect(455,85,60,60);
+  
+  noFill();
+  strokeWeight(3)
+  stroke(0,0,0)
+  bezier(-10,170,0,120,70,100,120,90,73,120);
+  bezier(-5,220,0,210,40,190,80,190);
+  
+  
+
+  
+  
+
+  
+  
+ 
+  
+ 
+	
+	
+	
+	
+
+} 
